5c465545d33b09ee98eeede41c1f9532d7a3af9b
Nathan Isaac <nathan@nathan-isaac.com>
2014-10-31 08:48:13 -0700

    Finish Chapter 8 problems 2 and 3

--- Chapter 8/Isaac_p2.s
+++ Chapter 8/Isaac_p2.s
@@ -25,24 +25,49 @@
 ; EQUATES
 ;---------------------------------------------------------------------------					
 
+VAL1A EQU 0xffffffff
+VAL2A EQU 0x00000001
+
+VAL1B EQU 0x0000000f
+VAL2B EQU 0x00000001
+
 ;---------------------------------------------------------------------------
 ; CODE 
 ;---------------------------------------------------------------------------
 					
-		AREA Sort, CODE
-		ENTRY
+stack_start EQU 0x40001000
+   AREA long_add, CODE
 
-stop	B	stop		; endless loop at end of program
+   ENTRY
+
+Start
+   	ldr sp, =stack_start ; Tell where we will place the stack.
+                        ; (It goes down (lower addresses from here.)
+	ldr R0, = VAL1A
+	ldr R1, = VAL1B
+
+	ldr R2, = VAL2A
+	ldr R3, = VAL2B
+
+   	bl long_add_subroutine
+
+stop b stop
+
+; This subroutine saves the registers,
+; messes up the registers locally,
+; then restores the registers and returns.
+long_add_subroutine
+   	stmfd sp!, {lr} ; save used registers and the link register (r14)
+   
+   	adds R8,R0,R1
+	adcs R9,R1,R3 
+   
+  	ldmfd sp!, {pc} ; pop the stack and return 
+	END 
 
 ;---------------------------------------------------------------------------
 ; DATA
 ;---------------------------------------------------------------------------
-
-ALIGN
-begin
-		DCD 0x00000001, 0x00000005, 0x00000002, 0x00000007, 0x00000006
-end_list	
-		END
 
 ;---------------------------------------------------------------------------
 ; End of file.
--- Chapter 8/Isaac_p3.s
+++ Chapter 8/Isaac_p3.s
@@ -29,10 +29,42 @@
 ; CODE 
 ;---------------------------------------------------------------------------
 					
-		AREA Sort, CODE
-		ENTRY
+stack_start EQU 0x40001000
+   AREA find_largest_subroutine, CODE
 
-stop	B	stop		; endless loop at end of program
+   ENTRY
+
+Start
+   	ldr sp, =stack_start ; Tell where we will place the stack.
+                        ; (It goes down (lower addresses from here.)
+
+	ldr r0,=end_list	; load the end address of the code & initialize it as the pointer.
+	ldr r2,=begin	; load the beginning address of the code.
+	mov r3,#0x00000000 ; Initialize r3 to 0
+
+   	bl find_largest_sub
+
+stop 
+	b stop
+
+; This subroutine saves the registers,
+; messes up the registers locally,
+; then restores the registers and returns.
+find_largest_sub
+   	stmfd sp!, {lr} ; save used registers and the link register (r14)
+   
+loop	
+	ldr r6,[r2],#4 ; load the next data into r6 and post increment r2 for the next data.
+	cmp r6,r3      ; Find out if r6 > r3. result from r6-r3  like subs r7,r6,r3 without r6 necessary.
+	
+	blt no_update  ; If it is no update.
+	mov r3,r6      ; update if r6 is higher than r3.
+
+no_update
+	cmp r0,r2      ;  are we at the end yet
+	bne loop       ; if r7 != 0 then keep looping
+   
+  	ldmfd sp!, {pc} ; pop the stack and return 
 
 ;---------------------------------------------------------------------------
 ; DATA
@@ -40,7 +72,7 @@ stop	B	stop		; endless loop at end of program
 
 ALIGN
 begin
-		DCD 0x00000001, 0x00000005, 0x00000002, 0x00000007, 0x00000006
+		DCD 0x00000001, 0x00000005, 0x00000111, 0x00000010, 0x00000006
 end_list	
 		END
 
